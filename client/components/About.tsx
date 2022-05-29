6b1731159e7d9138611c3e510bfb86a5df0402ef
Jake Bradbeer <jakebradbeer@gmail.com>
2022-05-29 15:44:23 -0700

    minor changes to about

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -8,21 +8,16 @@ export default function About () {
       ['https://bpiconsultants.ca/#:~:text=NAIOP%20Real%20Estate%20Excellence%20(REX)%20Award%20%2D%20Development%20of%20the%20Year',
        'NAIOP Real Estate Excellence (REX) Award - Development of the Year'],
       ['https://bpiconsultants.ca/files/Mississauga-News-REX-Award-Article.pdf',
-       'Ontario Concrete 2010 Award - Structural Design Innovation'],
+       'Ontario Concrete Award - Structural Design Innovation'],
       ['https://bpiconsultants.ca/files/Mississauga-Urban-Design-Award-Jury-ACV.pdf',
-       '2010 City of Mississauga Urban Design Award - Award of Excellence'],
+       'City of Mississauga Urban Design Award - Award of Excellence'],
     ];
 
     const awards:React.ReactElement[] = [];
 
     rawAwards.map(award => {
-      awards.push(
-        <li>
-          <a href={award[0]}>
-            {award[1]}
-          </a>
-        </li>);
-    });
+      awards.push(<li><a href={award[0]}>{award[1]}</a></li>);
+  });
 
     return awards;
   }
@@ -38,10 +33,10 @@ export default function About () {
           Founded in July 2000, BPI Consultants provides development management services for the real estate sector. The company’s principal is J. David Bradbeer, P.Eng., MBA. David has over 30 years experience in various branches of real estate and experience in all sectors of real estate including commercial, medical, industrial, institutional, residential, retail, LEED and P3’s.
           <br />
           <br />
-          Prior to founding BPI, David was with an international real estate consulting firm and with a mid-sized general contractor, design-builder and developer. David has a B.A.Sc. in Civil Engineering from the University of Waterloo (Professional Engineer) and an MBA from the University of Toronto (Rotman).
+          Prior to founding BPI, David was with an international real estate consulting firm and with a mid-sized general contractor, design-builder and developer. David has a B.A.Sc. in Civil Engineering from the University of Waterloo and an MBA from the University of Toronto (Rotman).
           <br />
           <br />
-          Many assignments are geared towards the development of new real estate although may also be for the benefit of an existing facility. Work can be completed prior to, during or post construction by the contractors. In the past 12-years, BPI Consultants has worked on over 9 million square feet of real estate projects with a value of over $500 million in both renovation and new construction work.
+          Many assignments are geared towards the development of new real estate although may also be for the benefit of an existing facility. Work can be completed prior to, during or post construction by the contractors. In the past Y-years, BPI Consultants has worked on over X million square feet of real estate projects with a value of over $Z million in both renovation and new construction work.
           <br />
           <br />
           <strong>We are proud to announce that our project, AeroCentre V, has won four prestigious industry awards:</strong>
