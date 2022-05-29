a83ddfb2a09688363a70d3d47d3abfba361c922e
Jake Bradbeer <jakebradbeer@gmail.com>
2022-05-29 15:42:51 -0700

    added reccomendation component, yet to be built out

--- client/components/Reccomendation.tsx
+++ client/components/Reccomendation.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
 
-export default function Clients () {
+export default function Reccomendation () {
   return (
-    <div id='clients'>
-      {/* OUR CLIENTS HEADER */}
-      <h3 className='section-header'> OURX CLIENTS </h3>
-
-      {/* RECCOMENDATIONS */}
-      <div id='reccomendations'>  
-        <p>dfsdf</p>
-      </div>
-
-      {/* CLIENT LIST */}
-      <div id='client-list'>
-      </div>
+    <div id='recomendation'>
     </div>
   )
 }
