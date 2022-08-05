b7f392057913d3f9ce872c5d5f1445e6f46adf9e
Jake Bradbeer <jakebradbeer@gmail.com>
2022-08-04 22:33:39 -0700

    added splitscreen to landing page

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,9 +1,20 @@
 import './Landing.scss';
+import gbIntLounge from './images/gb-int-lounge.jpeg';
 
 const Landing = () => {
   return (
     <section id='landing'>
-      <h1 id='landing-title'>REAL ESTATE DEVELOPMENT ADVISORY</h1>
+      <div className='cover'>
+        <h2 id='landing-title'>REAL ESTATE DEVELOPMENT ADVISORY</h2>
+        <h3>
+          BPI Consultants is a boutique consulting company specializing in advisory and management services for the
+          real estate and construction sectors.
+        </h3>
+        <button>
+          Contact Us
+        </button>
+      </div>
+      <img src={gbIntLounge} alt='background'/>
     </section>
   );
 }
